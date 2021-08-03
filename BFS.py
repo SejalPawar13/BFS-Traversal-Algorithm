315d6194b1a440bec94f75d0b446ce9f337d6210
SejalPawar13 <87138584+SejalPawar13@users.noreply.github.com>
2021-08-03 21:43:08 +0530

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,2 +1,32 @@
 # BFS-Traversal-Algorithm
- 
+ import queue as Q                  
+from RMP import dict_gn   #RMP.py is needed to be saved in Python files
+
+start='Arad'
+goal='Bucharest'
+result=''
+
+def BFS(city, cityq, visitedq):
+    global result
+    if city==start:
+        result=result+' '+city
+    for eachcity in dict_gn[city].keys():
+        if eachcity==goal:
+            result=result+' '+eachcity
+            return
+        if eachcity not in cityq.queue and eachcity not in visitedq.queue:
+            cityq.put(eachcity)
+            result=result+' '+eachcity
+    visitedq.put(city)
+    BFS(cityq.get(),cityq,visitedq)
+
+def main():
+    cityq=Q.Queue()
+    visitedq=Q.Queue()
+    BFS(start, cityq, visitedq)
+    print("BFS Traversal from ",start," to ",goal," is: ")
+    print(result)
+    
+main()
+
+
